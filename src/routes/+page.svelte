677cf23cfeb9f6c26fec0bd3b055be442b6b069e
Perttu Autti <65128229+Pertsaa@users.noreply.github.com>
2023-08-18 19:28:54 +0300

    feat: landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,53 @@
-<h1 class="text-3xl font-bold underline">buggyduck.dev</h1>
+<div class="bg-white dark:bg-neutral-950 py-24 sm:py-32">
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		<div class="mx-auto max-w-2xl">
+			<h2 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-50 sm:text-4xl">
+				From the blog
+			</h2>
+			<p class="mt-2 text-lg leading-8 dark:text-gray-300 text-gray-600">
+				If you were given the chance to bring your most brilliant ideas to life, what would you
+				create? This blog features articles by developers who seized the opportunity and turned
+				their ideas into 'serious' side projects.
+			</p>
+			<div class="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
+				<article class="flex max-w-xl flex-col items-start justify-between">
+					<div class="flex items-center gap-x-4 text-xs">
+						<time datetime="2020-03-16" class="text-gray-500 dark:text-gray-300">Aug 18, 2023</time>
+						<a
+							href="/blog/every-developer-needs-a-duck"
+							class="relative z-10 rounded-full bg-gray-50 dark:bg-neutral-900 px-3 py-1.5 font-medium text-gray-600 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800"
+							>Project</a
+						>
+					</div>
+					<div class="group relative">
+						<h3
+							class="mt-3 text-lg font-semibold leading-6 text-gray-900 dark:text-gray-50 group-hover:text-gray-600 dark:group-hover:text-gray-300"
+						>
+							<a href="/blog/every-developer-needs-a-duck">
+								<span class="absolute inset-0" />
+								Every developer needs a duck.
+							</a>
+						</h3>
+						<p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600 dark:text-gray-300">
+							You know those rubber ducks that developers use to explain their code? Well, I have a
+							real duck. His name is Quackers, and he's a good listener. He's also a good rubber
+							duck. He's a good duck in general. I love him.
+						</p>
+					</div>
+					<div class="relative mt-8 flex items-center gap-x-4">
+						<img src="#" alt="" class="h-10 w-10 rounded-full bg-gray-50" />
+						<div class="text-sm leading-6">
+							<p class="font-semibold text-gray-900 dark:text-gray-50">
+								<a href="/author/github-copilot">
+									<span class="absolute inset-0" />
+									GitHub Copilot
+								</a>
+							</p>
+							<p class="text-gray-600 dark:text-gray-300">Author</p>
+						</div>
+					</div>
+				</article>
+			</div>
+		</div>
+	</div>
+</div>
